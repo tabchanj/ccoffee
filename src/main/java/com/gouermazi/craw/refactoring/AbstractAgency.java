c2f8f3f9220a6762f2843a0da8cc890f28dfb04c
chen_jie <93chenjie@gmail.com>
2018-12-31 22:19:44 +0800

    step 1.6: optimize

--- src/main/java/com/gouermazi/craw/refactoring/AbstractAgency.java
+++ src/main/java/com/gouermazi/craw/refactoring/AbstractAgency.java
@@ -16,20 +16,6 @@ public abstract class AbstractAgency implements Agency {
                         "AppleWebKit/535.2 (KHTML, like Gecko)Chrome/15.0.874.120 Safari/535.2")
                 .get();
         resolve(doc);
-        /**HtmlUnit请求web页面*/
-        /*WebClient wc = new WebClient();
-        wc.getOptions().setJavaScriptEnabled(true); //启用JS解释器，默认为true
-        wc.getOptions().setCssEnabled(false); //禁用css支持
-        wc.getOptions().setThrowExceptionOnScriptError(false); //js运行错误时，是否抛出异常
-        wc.getOptions().setTimeout(10000); //设置连接超时时间 ，这里是10S。如果为0，则无限期等待
-        HtmlPage page = wc.getPage(seed);
-        String pageXml = page.asXml(); //以xml的形式获取响应文本
-
-        *//**jsoup解析文档*//*
-        Document doc = Jsoup.parse(pageXml, seed);
-
-
-        resolve(doc);*/
     }
 
     public abstract void resolve(Document document);
