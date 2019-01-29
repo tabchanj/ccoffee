b44bae0b053dde6da9282cc602c787abfabfaad8
chen_jie <93chenjie@gmail.com>
2019-01-29 10:32:37 +0800

    main

--- src/main/java/com/gouermazi/craw/Main.java
+++ src/main/java/com/gouermazi/craw/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        Agency agency = new HuntDog("/home/chen/xxx");
-        agency.takeDown("");
+        Agency agency = new LinkHunter("/home/chen/xxx");
+        agency.takeDown("http://www.hao6v.com/dy/2018-12-03/WuMingZhiBei.html");
     }
 }
